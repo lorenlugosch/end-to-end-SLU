8ca1d81457bfa00a8c8b452b006150759099b7ec
loren.lugosch <loren.lugosch@gmail.com>
2019-04-18 12:39:45 -0400

    move noise addition to collate function

--- data.py
+++ data.py
@@ -194,19 +194,19 @@ class SLUDataset(torch.utils.data.Dataset):
 		effect = torchaudio.sox_effects.SoxEffectsChain()
 		effect.set_input_file(wav_path)
 
-		if augment:
+		# if augment:
 			# speed/tempo
-			#min_speed = 0.9; max_speed = 1.1; speed_range = max_speed-min_speed
-			#speed = speed_range * np.random.rand(1)[0] + min_speed
-			#effect.append_effect_to_chain("tempo", speed)
-			#del speed
+			# min_speed = 0.9; max_speed = 1.1; speed_range = max_speed-min_speed
+			# speed = speed_range * np.random.rand(1)[0] + min_speed
+			# effect.append_effect_to_chain("tempo", speed)
+			# del speed
 
 			# volume
-			min_gain = -10; max_gain = 10; gain_range = max_gain-min_gain
-			gain_dB = gain_range * np.random.rand(1)[0] + min_gain
-			gain = 10**(gain_dB/20)
-			effect.append_effect_to_chain("vol", gain)
-			del gain_dB
+			#min_gain = -10; max_gain = 10; gain_range = max_gain-min_gain
+			#gain_dB = gain_range * np.random.rand(1)[0] + min_gain
+			#gain = 10**(gain_dB/20)
+			#effect.append_effect_to_chain("vol", gain)
+			#del gain_dB
 
 		wav, fs = effect.sox_build_flow_effects()
 		x = wav[0].numpy()
@@ -239,32 +239,32 @@ class CollateWavsSLU:
 		for index in range(batch_size):
 			x_,y_intent_,index_ = batch[index]
 
-			# augment = self.augment and (index_ / self.len_df) > 1
-			# if augment:
-			# 	# crop
-			# 	min_length = round(x_.shape[0]*0.9); max_length = round(x_.shape[0]*1.1); length_range=max_length-min_length
-			# 	length = int(length_range * np.random.rand(1)[0] + min_length)
-			# 	start = int((x_.shape[0]-length)/2)
-			# 	if start < 0:
-			# 		left_padding = -start
-			# 		right_padding = length-(x_.shape[0]-start)
-			# 		x_ = np.pad(x_,(left_padding, right_padding),mode="constant")
-			# 	else:
-			# 		start += np.random.randint(low=-start, high=1, size=1)[0]
-			# 		x_ = x_[start:start+length]
-			# 	length = x_.shape[0]
+			augment = self.augment and (index_ / self.len_df) > 1
+			if augment:
+				# crop
+				min_length = round(x_.shape[0]*0.9); max_length = round(x_.shape[0]*1.1); length_range=max_length-min_length
+				length = int(length_range * np.random.rand(1)[0] + min_length)
+				start = int((x_.shape[0]-length)/2)
+				if start < 0:
+					left_padding = -start
+					right_padding = length-(x_.shape[0]-start)
+					x_ = np.pad(x_,(left_padding, right_padding),mode="constant")
+				else:
+					start += np.random.randint(low=-start, high=1, size=1)[0]
+					x_ = x_[start:start+length]
+				length = x_.shape[0]
 
-			# 	# noise (taken from https://github.com/jfsantos/maracas/blob/master/maracas/maracas.py)
-			# 	noise = np.random.choice(self.noises, 1, p=[1/len(self.noises) for _ in range(len(self.noises))])[0]
-			# 	snr = np.random.choice(self.SNRs, 1, p=[1/len(self.SNRs) for _ in range(len(self.SNRs))])[0]
-			# 	start = np.random.randint(low=0, high=len(noise)-length, size=1)[0]
-			# 	end = start + length
-			# 	noise = noise[start:end]
-			# 	N_dB = rms_energy(noise)
-			# 	S_dB = rms_energy(x_)
-			# 	N_new = S_dB - snr
-			# 	noise_scaled = 10**(N_new/20) * noise / 10**(N_dB/20)
-			# 	x_ = x_ + noise_scaled
+				# noise (taken from https://github.com/jfsantos/maracas/blob/master/maracas/maracas.py)
+				noise = np.random.choice(self.noises, 1, p=[1/len(self.noises) for _ in range(len(self.noises))])[0]
+				snr = np.random.choice(self.SNRs, 1, p=[1/len(self.SNRs) for _ in range(len(self.SNRs))])[0]
+				start = np.random.randint(low=0, high=len(noise)-length, size=1)[0]
+				end = start + length
+				noise = noise[start:end]
+				N_dB = rms_energy(noise)
+				S_dB = rms_energy(x_)
+				N_new = S_dB - snr
+				noise_scaled = 10**(N_new/20) * noise / 10**(N_dB/20)
+				x_ = x_ + noise_scaled
 
 			x.append(torch.tensor(x_).float())
 			y_intent.append(torch.tensor(y_intent_).long())
